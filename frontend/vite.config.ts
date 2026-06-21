9846003dbd7320870689e1c071c680fbf7e1721d
Benard2798 <thairukibe2798@gmail.com>
2026-06-21 14:38:29 +0300

    Fix vercel deployment configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/swift_crest_safaris_final/',
+  base: "/",
   plugins: [react()],
   server: {
     port: 5173,
